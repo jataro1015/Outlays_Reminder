9630c098cb9cca111c8e1610f0c4d8e24d9b761f
jataro1015 <jataro.10.15@gmail.com>
2025-03-01 03:56:48 +0900

    Initial Commit

---
+++ backend/src/main/java/jataro/web/outlays_reminder/OutlaysReminderApplication.java
@@ -0,0 +1,13 @@
+package jataro.web.outlays_reminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutlaysReminderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutlaysReminderApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/jataro/web/outlays_reminder/OutlaysReminderApplicationTests.java
@@ -0,0 +1,13 @@
+package jataro.web.outlays_reminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutlaysReminderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
