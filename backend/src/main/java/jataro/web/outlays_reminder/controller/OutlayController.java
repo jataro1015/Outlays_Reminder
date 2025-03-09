49e36bf4afc8a6de60782349d98a2af91b4a493c
jataro1015 <jataro.10.15@gmail.com>
2025-03-09 20:25:18 +0900

    20250309_2025

--- backend/src/main/java/jataro/web/outlays_reminder/controller/OutlayController.java
+++ backend/src/main/java/jataro/web/outlays_reminder/controller/OutlayController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -31,7 +32,7 @@ public final class OutlayController {
 		final Integer amount = (Integer) requestBody.get("amount");
 		
 		try {
-			
+			 
 			Outlay outlay = Outlay.create(item, amount);
 			Outlay savedOutlay = outlayRepository.save(outlay);
 			return ResponseEntity.ok(savedOutlay);
@@ -39,6 +40,16 @@ public final class OutlayController {
 		} catch (IllegalArgumentException e) {
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> getOutlayById(@PathVariable("id") Integer id){
+		final Optional<Outlay> existingOutlay = outlayRepository.findById(id); // IDで既存の Outlay を検索
+        if (existingOutlay.isEmpty()) {
+            return new ResponseEntity<>("指定されたIDの出費データは存在しません。", 
+            		HttpStatus.NOT_FOUND); // 404 Not Found を返す
+        }
+		return new ResponseEntity<>(existingOutlay.get(), HttpStatus.OK);
 	}
 	
 	@PutMapping("/{id}")
